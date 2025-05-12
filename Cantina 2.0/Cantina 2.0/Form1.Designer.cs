1dd900d89cc4bda53e91a8cb753b6bfc6e245423
Isabella751 <isabella.leite562@gmail.com>
2025-05-12 10:20:28 -0300

    Cantina 2.0

---
+++ Cantina 2.0/Cantina 2.0/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Cantina_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtCantina = new Label();
+            SuspendLayout();
+            // 
+            // txtCantina
+            // 
+            txtCantina.AutoSize = true;
+            txtCantina.Location = new Point(341, 30);
+            txtCantina.Name = "txtCantina";
+            txtCantina.Size = new Size(76, 15);
+            txtCantina.TabIndex = 0;
+            txtCantina.Text = "Cantina Ultra";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtCantina);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label txtCantina;
+    }
+}
